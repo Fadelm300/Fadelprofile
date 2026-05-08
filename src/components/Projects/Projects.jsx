a86833d23e43412162b804719ef70824b59033c4
fadelm <fadel.m200@gmail.com>
2026-05-09 00:30:02 +0300

    Add InsightBoard project and center project cards layout

--- src/components/Projects/Projects.jsx
+++ src/components/Projects/Projects.jsx
@@ -7,33 +7,77 @@ import quizAppImg from "../../assets/Projectsimg/quizapp.png";
 import carsBlog from "../../assets/Projectsimg/carsBlog.png";
 import xoaiImg from "../../assets/Projectsimg/xo-AI.png";
 import cardMatchImg from "../../assets/Projectsimg/cardmatch.png";
+import EmployeeManagementSystem from "../../assets/Projectsimg/EM_img.png";
+import insightBoardImg from "../../assets/Projectsimg/insightboard.png";
+
 const Projects = () => {
   const [ref, isVisible] = useScrollAnimation();
 
   const projects = [
-    ,
-    { name: "Blog", img: carsBlog, link: "https://github.com/Fadelm300/Car-Blogs" },
-    { name: "UniClub", img: uniclubImg, link: "https://github.com/Fadelm300/UniClub-frontend" },
-    { name: "Quiz App", img: quizAppImg, link: "https://github.com/HaroonAlnhdi/Quizz_App" },
-    { name: "Card Match", img: cardMatchImg, link: "https://github.com/Fadelm300/Memory_cards_Game" },
-    { name: "XO AI", img: xoaiImg, link: "https://github.com/Fadelm300/tic-tac-toe" },
-   { name: "Pharmacy", img: pharmacyImg, link: "https://github.com/Fadelm300/pharmacy_489" }
+    {
+      name: "UniClub",
+      img: uniclubImg,
+      link: "https://github.com/Fadelm300/UniClub-frontend",
+    },
+    {
+      name: "Employee Management System",
+      img: EmployeeManagementSystem,
+      link: "https://github.com/Fadelm300/EmployeeManagementSystem-frontend",
+    },
+    {
+      name: "InsightBoard",
+      img: insightBoardImg,
+      link: "https://github.com/Fadelm300/insightboard",
+    },
+    {
+      name: "Blog",
+      img: carsBlog,
+      link: "https://github.com/Fadelm300/Car-Blogs",
+    },
+    {
+      name: "Quiz App",
+      img: quizAppImg,
+      link: "https://github.com/HaroonAlnhdi/Quizz_App",
+    },
+    {
+      name: "Card Match",
+      img: cardMatchImg,
+      link: "https://github.com/Fadelm300/Memory_cards_Game",
+    },
+    {
+      name: "XO AI",
+      img: xoaiImg,
+      link: "https://github.com/Fadelm300/tic-tac-toe",
+    },
+    {
+      name: "Pharmacy",
+      img: pharmacyImg,
+      link: "https://github.com/Fadelm300/pharmacy_489",
+    },
   ];
 
   return (
-      <section ref={ref}    className={`projects-section${isVisible ? " animate" : ""}`}>
-
+    <section
+      ref={ref}
+      className={`projects-section${isVisible ? " animate" : ""}`}
+    >
       <div className="projects-container">
         <h2 className="projects-title">Projects</h2>
+
         <p className="projects-description">
           I have worked on a wide range of projects, from web apps to Android apps.
           Here are some of my latest projects.
         </p>
+
         <div className="projects-grid">
           {projects.map((project, index) => (
             <div key={index} className="project-card">
               <a href={project.link} target="_blank" rel="noopener noreferrer">
-                <img src={project.img} alt={project.name} className="project-image" />
+                <img
+                  src={project.img}
+                  alt={project.name}
+                  className="project-image"
+                />
                 <span className="project-name">{project.name}</span>
               </a>
             </div>
@@ -45,9 +89,3 @@ const Projects = () => {
 };
 
 export default Projects;
-
-
-
-
-
-
