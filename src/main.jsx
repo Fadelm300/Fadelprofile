892e5b7f1d4e3170c0746e53a52319e87d78fa9f
fadelm <fadel.m200@gmail.com>
2026-07-21 17:53:29 +0300

    self-host fonts to improve page loading

--- src/main.jsx
+++ src/main.jsx
@@ -1,3 +1,10 @@
+import "@fontsource/dm-sans/latin-300.css";
+import "@fontsource/dm-sans/latin-300-italic.css";
+import "@fontsource/dm-sans/latin-400.css";
+import "@fontsource/dm-sans/latin-500.css";
+import "@fontsource/dm-serif-display/latin-400.css";
+import "@fontsource/dm-serif-display/latin-400-italic.css";
+
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
